84ed8e11517c52c7068996200eaa982f3cdcd97a
Vipul Kumar <tovipul.kr@gmail.com>
2025-11-14 04:53:12 +0530

    commit

---
+++ Frontend/src/pages/Orders.jsx
@@ -0,0 +1,96 @@
+
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
+import axios from 'axios';
+
+const Orders = () => {
+  const { backendUrl, token, currency } = useContext(ShopContext);
+  const [orderData, setOrderData] = useState([])
+
+  const loadOrderData = async () => {
+    try {
+      if (!token) {
+        return null
+      }
+
+      const response = await axios.post(backendUrl + '/api/order/userOrders', {}, {
+        headers: {
+          Authorization: `Bearer ${token}`  // <-- Header with token
+        }
+      }
+      )
+      if(response.data.success){
+        let allOrdersItem = []
+        response.data.orders.map((order)=>{
+          order.items.map((item)=>{
+            item['status']= order.status
+            item['payment'] = order.payment
+            item['paymentMethod']= order.paymentMethod
+            item['date']= order.date
+            allOrdersItem.push(item);
+          })
+        }) 
+        setOrderData(allOrdersItem.reverse())
+      }
+      
+    }
+    catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    loadOrderData()
+  }, [token])
+
+  return (
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+        <Title text1={'MY'} text2={' ORDERS'} />
+      </div>
+      <div>
+        {orderData.map((item, index) => (
+          <div
+            key={index}
+            className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:text-center md:justify-between gap-4'
+          >
+
+            <div className='flex items-start gap-6 text-sm md:w-1/2'>
+              <img src={item.image[0]} className='w-16 sm:w-20' />
+              <div className='flex flex-col items-start'>
+                <p className='sm:text-base font-medium'>{item.name}</p>
+                <div className='flex items-center gap-3 mt-1 text-base text-gray-700'>
+                  <p>{currency}{item.price}</p>
+                  <p>Quantity : {item.quantity}</p>
+                  <p>Size: {item.size}</p>
+
+                </div>
+
+                <p className='mt-1'>Date: <span className='text-gray-400'>{new Date(item.date).toDateString()}</span></p>
+                <p className='mt-2'>Payment: <span className='text-gray-400'>{(item.paymentMethod)}</span></p>
+
+              </div>
+            </div>
+
+
+            <div className='md:w-1/2 flex justify-between items-center'>
+              <div className='flex items-center gap-2'>
+                <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
+                <p className='text-sm md:text-base'>{item.status}</p>
+              </div>
+
+
+              <button className='border px-4 py-1 sm:px-6 sm:py-2 md:px-8 md:py-3 text-sm sm:text-base font-medium rounded-sm flex items-center'
+              onClick={loadOrderData}>
+                Track order
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
