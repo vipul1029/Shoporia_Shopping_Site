490450f3a6b60d7d038d8e8ae9f7f106185afdd3
Vipul Kumar <tovipul.kr@gmail.com>
2025-12-09 00:44:00 +0530

    Refactor MongoDB connection setup

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,12 +1,26 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+
+//   mongoose.connection.on('connected', ()=>{
+//     console.log("DB connected");
+//   })
+  
+//   await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+// }
+
+// export default connectDB;
+
+
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-  mongoose.connection.on('connected', ()=>{
+  mongoose.connection.on("connected", () => {
     console.log("DB connected");
-  })
-  
-  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-}
+  });
+
+  await mongoose.connect(process.env.MONGO_URI);
+};
 
 export default connectDB;
